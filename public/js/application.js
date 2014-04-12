a3b4705ce63193e8ef6fa3772874e2312bbc20f3
Apprentice <apprentice@dbc13.local>
2014-04-11 19:10:52 -0700

    Implemented the counter for questions and answers

--- public/js/application.js
+++ public/js/application.js
@@ -1,30 +1,36 @@
-
-//View JS
-
-function View() {
-  this.buttonWhichDoesSomething = $('#add_question');
-
-  this.add_question = function () {
-    $('#questionbtn').append("<br><input type='text' name='question[0]' placeholder='Question'>")
-  };
-
-  this.add_answer = function () {
-    $('#addAnswerBtn').append('<br><input type="text" name="answer[0]" placeholder="Answer">')
-  };
-}
-
-//Controller
-
-function Controller (view) {
-  this.view = view;
-  $('#questionbtn').click(this.view.add_question);
-  $('#addAnswerBtn').click(this.view.add_answer);
-}
-
 //Application JS
 
 $(document).ready(function() {
   var view = new View()
   var controller = new Controller(view)
 });
+
+//View JS
+
+function View() {
+  event.preventDefault();
+  var questionCount = 0;
+  var answerCount = 0;
+
+  View.prototype.add_question = function () {
+    $('#addQuestionBtn').append('<br><input type="text" name="question[' + this.questionCount + ']" placeholder="Question">');
+    questionCount++;
+    console.log(questionCount)
+  };
+
+  View.prototype.add_answer = function () {
+    $('#addAnswerBtn').append('<br><input type="text" name="answer[' + this.answerCount + '] placeholder="Answer">');
+    answerCount++;
+    console.log(answerCount)
+  }
+}
+
+//Controller
+
+function Controller (view) {
+  this.view = view;
+  $('#addQuestionBtn').click(this.view.add_question);
+  $('#addAnswerBtn').click(this.view.add_answer);
+}
+
 
