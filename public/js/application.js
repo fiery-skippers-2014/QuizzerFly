09ab77a7f3a69c7584aa53dd69ac1e9f8dcf652c
Apprentice <apprentice@dbc14.local>
2014-04-12 14:59:34 -0700

    Got adding new questions and answers working with correctly with incrementing numbers   *added new event listener each tied to each new question for the new choices   *moved new choice button below each question   *fixed all id and names for the forms to identify each question and answer that it is tied to
    TODO: Identify way to save it to database using the id and names from each form input
      *possibly adjust the input names to better fit a params hash
      *eventually refactor the messy functions
      *write a test

--- public/js/application.js
+++ public/js/application.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
   var view = new View()
   view.first_questions()
   var controller = new Controller(view)
-
 });
+
+
+
 
 //View JS
 
@@ -18,56 +20,48 @@ function View() {
     var questionStartingCount = 1;
     var $node = '';
     for(varCount=0;varCount<questionStartingCount;varCount++){
-      console.log("This is the var_Count: " + varCount)
+    var answerCount = 2
       var displayCount = varCount+1;
-
-      $node += '<div class="question" id="question[' + displayCount + ']"><div id="questionInput"><input type="text" name="question[' + displayCount + ']" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[' + displayCount + '][answer[0]"] placeholder="Answer"><br><input type="text" name="question[' + displayCount + '][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a>'
+      $node += '<div class="question" id="question[' + displayCount + ']"><div id="questionInput"><input type="text" name="question[' + displayCount + ']" placeholder="Question"><br></div><div id="answerInputs1"><input type="text" name="question[1]answer[1]" placeholder="Answer"><br><input type="text" name="question[1]answer[2]" placeholder="Answer"><br></div><a class="addAnswerBtn" id="addAnswerBtn1" href="#"> Add Answer</a>';
 
     }
 
-    $('#questions').prepend($node);
+    $('#questions').append($node);
   }
 
-  View.prototype.add_question = function() {
+  View.prototype.addAnswer = function () {
+    var currentChoiceInputs = $(this).prev()
+   console.log("This is the number of children for current choice inputs: " + $(this).prev().children('input').length)
+var next_input_count = $(this).prev().children('input').length + 1
+    $node = '<input type="text" name="question[1]answer[' + next_input_count + ']" placeholder="Answer"><br>'
+
+     currentChoiceInputs.append($node);
+  }
+
+
+  View.prototype.addQuestion = function() {
     varCount++;
-    $node = '<div class="question" id="question[' + varCount + ']"><div id="questionInput"><input type="text" name="question[' + varCount + ']" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[' + varCount + '][answer[0]"] placeholder="Answer"><br><input type="text" name="question[' + varCount + '][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a>';
-    $('#questions').after($node);
+    $node = '<div class="question" id="question[' + varCount + ']"><div id="questionInput"><input type="text" name="question[' + varCount + ']" placeholder="Question"><br></div><div id="answerInputs' + varCount + '"><input type="text" name="question[' + varCount + ']answer[1]"] placeholder="Answer"><br><input type="text" name="question[' + varCount + ']answer[2]"] placeholder="Answer"><br></div><a class="addAnswerBtn" id="addAnswerBtn' +varCount + '" href="#"> Add Answer</a>';
+
+    $('#questions').append($node);
+
+    $('#addAnswerBtn' + varCount).unbind().click(function () {
+        var question_id = $(this).parents()[0].id
+        var currentChoiceInputs = $(this).prev()
+        var next_input_count = $(this).prev().children('input').length + 1
+        $node = '<input type="text" name="' + question_id+ 'answer[' + next_input_count + ']" placeholder="Answer"><br>'
+        currentChoiceInputs.append($node);
+        });
   };
 
-
-  View.prototype.add_answer = function () {
-    $node = '<input type="text" name="answer[' + this.answerCount + ']" placeholder="Answer"><br>'
-    // $('#answerInput').append('<input type="text" name="answer[' + this.answerCount + ']" placeholder="Answer"><br>');
-    answerCount++;
-     console.log(answerCount)
-    $(this).parent().after($node);
-  }
 }
 
 //Controller
 
 function Controller (view) {
   this.view = view;
-  console.log($('#addQuestionBtn'))
-
-  $('#addQuestionBtn').unbind().click(this.view.add_question);
-  $('#addAnswerBtn').unbind().click(this.view.add_answer);
+  $('#addQuestionBtn').unbind().click(this.view.addQuestion);
+  $('#addAnswerBtn1').unbind().click(this.view.addAnswer);
 }
 
 
-// function View() {
-
-//   event.preventDefault();
-//   var answerCount = 0;
-//   View.prototype.first_questions = function () {
-//   var questionStartingCount = 1;
-//   var $node = "";
-//   console.log("First questions running")
-//   for(varCount=0;varCount<=questionStartingCount;varCount++){
-//     console.log("This is the var_Count: " + varCount)
-//     var displayCount = varCount+1;
-//     $node += '<div class="question" id="question' + displayCount + '"><div id="questionInput"><input type="text" name="question[0]" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a><br><a id="addQuestionBtn" href="#"> Add Question</a></div>'
-//     console.log("This is the displayCount" + displayCount)
-//   }
-
-  // $('#addQuestionBtn').unbind().click(this.view.add_question);
