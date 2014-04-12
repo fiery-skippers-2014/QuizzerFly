5da01e596b14f76e8d028d06a93f95aad2029aa3
Apprentice <apprentice@dbc14.local>
2014-04-12 12:14:05 -0700

    Can add questions with two choices and correctly increment the question id for the class and the input name.   *removed initial question html from create_survey.erb file   *changed the add question button to append directly to questions div and not to   parent of button   *moved add question button outside of questions div in order for it to always be at bottom last question   *moved the rendering of the first question above the initializating of controller in order to allow   button to exist so it can add an event listener to it.
    TODO: add abitlity to add addition choices.  Save complete form into database.

--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,9 @@
 
 $(document).ready(function() {
   var view = new View()
-  var controller = new Controller(view)
   view.first_questions()
+  var controller = new Controller(view)
+
 });
 
 //View JS
@@ -12,28 +13,34 @@ function View() {
 
   event.preventDefault();
   var answerCount = 0;
-  View.prototype.first_questions = function () {
-  var questionStartingCount = 3;
-  var $node = "";
-  for(varCount=0;varCount<=questionStartingCount;varCount++){
-    var displayCount = varCount+1;
-    $node += '<div class="question" id="question' + displayCount + '"><div id="questionInput"><input type="text" name="question[0]" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a><br><a id="addQuestionBtn" href="#"> Add Question</a></div>'
-  }
- 
-  // $(#questions).prepend($node);
-}
 
-View.prototype.add_question = function() {
-varCount++;
-$node = '<div class="question" id="question' + varCount + '"><div id="questionInput"><input type="text" name="question[0]" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a><br><a id="addQuestionBtn" href="#"> Add Question</a></div>';
-$(this).parent().before($node);
-};
+  View.prototype.first_questions = function () {
+    var questionStartingCount = 1;
+    var $node = '';
+    for(varCount=0;varCount<questionStartingCount;varCount++){
+      console.log("This is the var_Count: " + varCount)
+      var displayCount = varCount+1;
+
+      $node += '<div class="question" id="question[' + displayCount + ']"><div id="questionInput"><input type="text" name="question[' + displayCount + ']" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[' + displayCount + '][answer[0]"] placeholder="Answer"><br><input type="text" name="question[' + displayCount + '][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a>'
+
+    }
+
+    $('#questions').prepend($node);
+  }
+
+  View.prototype.add_question = function() {
+    varCount++;
+    $node = '<div class="question" id="question[' + varCount + ']"><div id="questionInput"><input type="text" name="question[' + varCount + ']" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[' + varCount + '][answer[0]"] placeholder="Answer"><br><input type="text" name="question[' + varCount + '][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a>';
+    $('#questions').after($node);
+  };
 
 
   View.prototype.add_answer = function () {
-    $('#answerInput').append('<input type="text" name="answer[' + this.answerCount + ']" placeholder="Answer"><br>');
+    $node = '<input type="text" name="answer[' + this.answerCount + ']" placeholder="Answer"><br>'
+    // $('#answerInput').append('<input type="text" name="answer[' + this.answerCount + ']" placeholder="Answer"><br>');
     answerCount++;
      console.log(answerCount)
+    $(this).parent().after($node);
   }
 }
 
@@ -41,8 +48,26 @@ $(this).parent().before($node);
 
 function Controller (view) {
   this.view = view;
+  console.log($('#addQuestionBtn'))
+
   $('#addQuestionBtn').unbind().click(this.view.add_question);
   $('#addAnswerBtn').unbind().click(this.view.add_answer);
 }
 
 
+// function View() {
+
+//   event.preventDefault();
+//   var answerCount = 0;
+//   View.prototype.first_questions = function () {
+//   var questionStartingCount = 1;
+//   var $node = "";
+//   console.log("First questions running")
+//   for(varCount=0;varCount<=questionStartingCount;varCount++){
+//     console.log("This is the var_Count: " + varCount)
+//     var displayCount = varCount+1;
+//     $node += '<div class="question" id="question' + displayCount + '"><div id="questionInput"><input type="text" name="question[0]" placeholder="Question"><br></div><div id="answerInput"><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br><input type="text" name="question[0][answer[0]"] placeholder="Answer"><br></div><a id="addAnswerBtn" href="#"> Add Answer</a><br><a id="addQuestionBtn" href="#"> Add Question</a></div>'
+//     console.log("This is the displayCount" + displayCount)
+//   }
+
+  // $('#addQuestionBtn').unbind().click(this.view.add_question);
