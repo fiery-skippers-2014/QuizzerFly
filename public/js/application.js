535e28d2d7f538c7319133fef7f8ada35803eba8
Apprentice <apprentice@dbc13.local>
2014-04-11 18:47:49 -0700

    Question button and Answer buttons work dynamically

--- public/js/application.js
+++ public/js/application.js
@@ -6,8 +6,10 @@ function View() {
 
   this.add_question = function () {
     $('#questionbtn').append("<br><input type='text' name='question[0]' placeholder='Question'>")
+  };
 
-
+  this.add_answer = function () {
+    $('#addAnswerBtn').append('<br><input type="text" name="answer[0]" placeholder="Answer">')
   };
 }
 
@@ -16,6 +18,7 @@ function View() {
 function Controller (view) {
   this.view = view;
   $('#questionbtn').click(this.view.add_question);
+  $('#addAnswerBtn').click(this.view.add_answer);
 }
 
 //Application JS
@@ -23,6 +26,5 @@ function Controller (view) {
 $(document).ready(function() {
   var view = new View()
   var controller = new Controller(view)
-
 });
 
