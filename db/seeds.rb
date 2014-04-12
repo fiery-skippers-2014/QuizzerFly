74450113e553ae2f3e6c2fb870fa12e1ce1f7758
Alexandra Swart <answart@sbcglobal.net>
2014-04-11 18:35:19 -0700

    A User can view all surveys
    * Allow all surveys to be accessable on the Home page
    * Refactor routes to allow all surveys to be shown on homepage
    
    TODO: A User can view a Survey Page

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+5.times do
+  @this_user = User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => 'password'
+
+  2.times do
+    Survey.create :user_id => @this_user.id, :title => Faker::Commerce.color, :description => Faker::Company.bs
+  end 
+end
