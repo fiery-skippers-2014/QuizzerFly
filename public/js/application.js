63569d1dfd76404dc21505ccfe0a63944ef38f2b
Apprentice <apprentice@dbc14.local>
2014-04-12 23:32:08 -0700

    Refactored all Javascript fo make it more MVC   *Cleaned it all up   *It's so pretty
    TODO: Stop working in project and do challenges

--- public/js/application.js
+++ public/js/application.js
@@ -5,14 +5,15 @@ $(document).ready(function() {
   var controller = new Controller(view)
 });
 
-//View JS
-function Question(question_count) {
-  this.node = '<div class="question" id="question' + question_count + '"><div id="questionInput"><input type="text" name="question' + question_count + '" placeholder="Question"><br></div><div id="answerInputs' + question_count + '"></div><a class="addAnswerBtn" id="addAnswerBtn' +question_count + '" href="#"> Add Choice</a>'
+//Controller
+
+function Controller (view) {
+  this.view = view;
+   $('#addQuestionBtn').click(this.view.addQuestion);
+    $('#questions').on('click','.addAnswerBtn',this.view.addChoice)
 }
 
-function Choice(question_id,choice_number) {
-  this.node = '<input type="text" name="' + question_id+ 'choice' + choice_number + '" placeholder="Choice"><br>'
-}
+//View JS
 
 View = function(){
  event.preventDefault();
@@ -20,46 +21,30 @@ View = function(){
 }
 
 View.prototype = {
-  addQuestion: function() {
+  addQuestion: function(e) {
+    e.preventDefault();
     questionCount++;
     var question = new Question(questionCount)
-    $('#questions').append(question.node);
-    // $('#addAnswerBtn' + questionCount).click(
-    //   function () {
-    //     var question_id = $(this).parents()[0].id
-    //     var currentChoiceInputs = $(this).prev()
-    //     var choice_number = currentChoiceInputs.children('input').length + 1
-    //     var choice = new Choice(question_id, choice_number)
-    //     currentChoiceInputs.append(choice.node);
-    //     });}
-  // func2: function(){}
-}
+    $('#questions').append(question.node);},
 
-
-
-//Controller
-
-function Controller (view) {
-  this.view = view;
-  $('#addQuestionBtn').click(this.view.addQuestion);
-
-
+  addChoice: function (e) {
+    e.preventDefault();
+    var $this = $(e.target)
+    var question_id = $this.parents()[0].id
+    var currentChoiceInputs = $this.prev()
+    var choice_number = currentChoiceInputs.children('input').length + 1
+    var choice = new Choice(question_id, choice_number)
+    currentChoiceInputs.append(choice.node);
+  }
 
 }
 
+function Question(question_count) {
+  this.node = '<div class="question" id="question' + question_count + '"><div id="questionInput"><input type="text" name="question' + question_count + '" placeholder="Question"><br></div><div id="answerInputs"></div><a class="addAnswerBtn" id="addAnswerBtn" href="#"> Add Choice</a>'
+}
 
-    $('#addAnswerBtn' + questionCount).click(
-      function () {
-        var question_id = $(this).parents()[0].id
-        var currentChoiceInputs = $(this).prev()
-        var choice_number = currentChoiceInputs.children('input').length + 1
-        var choice = new Choice(question_id, choice_number)
-        currentChoiceInputs.append(choice.node);
-        });}
+function Choice(question_id,choice_number) {
+  this.node = '<input type="text" name="' + question_id+ 'choice' + choice_number + '" placeholder="Choice"><br>'
+}
 
-  // View.prototype.addAnswer = function () {
-  //   var currentChoiceInputs = $(this).prev()
-  //   var next_input_count = $(this).prev().children('input').length + 1
-  //   $node = '<input type="text" name="question1choice' + next_input_count + '" placeholder="Choice"><br>'
-  //    currentChoiceInputs.append($node);
-  // }
+
