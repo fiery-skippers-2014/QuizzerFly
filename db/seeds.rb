3815d9b912638756734c23b3c27bc50ded124c12
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-12 20:48:25 -0700

    Got my seeds back

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@
   @this_user = User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => 'password'
 
   2.times do
-    Survey.create :user_id => @this_user.id, :title => Faker::Commerce.color, :description => Faker::Company.bs
+    @this_survey = Survey.create :user_id => @this_user.id, :title => Faker::Commerce.color, :description => Faker::Company.bs
+
+    5.times do
+      @this_question = Question.create :survey_id => @this_survey.id, :question_text => Faker::Lorem.sentence
+
+      4.times do
+        Choice.create :question_id => @this_question.id, :choice_text => Faker::Name.first_name
+      end
+    end
   end
 end
