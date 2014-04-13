6a9c1ff29034d01a3e7878d083ab4fbdd1172881
Apprentice <apprentice@dbc14.local>
2014-04-12 22:17:43 -0700

    Refactored JS and other parts   *pulled the question out as their own class in view   *pulled choice out as its own class in view   *rendered questions separately
    TODO: move the event listner for the choice addition to the controller

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -3,26 +3,16 @@
 
 # Not necessary since we have create survey partial on profile
 # Send to create_survey form
+
 post '/surveys/new' do
  @survey = Survey.create(
   title: params[:title],
   user_id: current_user.id,
   description: params[:description])
  @survey.save
- puts params
  @survey.build(params)
   redirect "/"
 end
-
-
-# # Create survey from create_survey form
-# post '/surveys/create' do
-#   @survey = Survey.create(params)
-#   redirect "/surveys/#{@survey.id}"
-# end
-
-
-
 
 # Show survey by survey id
 get "/surveys/:survey_id" do
@@ -30,10 +20,6 @@ get "/surveys/:survey_id" do
   @user = User.find(@survey.user_id)
   erb :'/survey/show_one'
 end
-
-
-
-
 
 # Allow a user to delete their own survey then return to their profile
 get '/surveys/:survey_id/delete' do
@@ -43,13 +29,12 @@ get '/surveys/:survey_id/delete' do
   redirect "/users/#{@user.id}"
 end
 
-
-
 # GO TO SURVEY FORM TO TAKE SURVEY
 get '/surveys/:survey_id/result/new' do
   @survey = Survey.find(params[:survey_id])
   erb :'completed_surveys/take_survey'
 end
+
 # Create FILLED OUT SURVEY
 post '/surveys/:survey_id/result' do
   @survey = Survey.find(params[:survey_id])
