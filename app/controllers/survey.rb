889287774c370b2c58de857cea645b67fd3c325b
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-13 13:11:22 -0700

    show one erb now shows all the questions and the results that correspond with those questions

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -17,6 +17,7 @@ end
 get "/surveys/:survey_id" do
   @survey = Survey.find(params[:survey_id])
   @user = User.find(@survey.user_id)
+
   erb :'/survey/show_one'
 end
 ##############################################################
