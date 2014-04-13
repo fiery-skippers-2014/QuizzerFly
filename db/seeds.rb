621c7aa0a43b90468ece9228a836c2c14cab0173
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-13 12:43:37 -0700

    removed seed data for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-5.times do
-  @this_user = User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => 'password'
+# 5.times do
+#   @this_user = User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => 'password'
 
-  2.times do
-    @this_survey = Survey.create :user_id => @this_user.id, :title => Faker::Commerce.color, :description => Faker::Company.bs
+#   2.times do
+#     @this_survey = Survey.create :user_id => @this_user.id, :title => Faker::Commerce.color, :description => Faker::Company.bs
 
-    5.times do
-      @this_question = Question.create :survey_id => @this_survey.id, :question_text => Faker::Lorem.sentence
+#     5.times do
+#       @this_question = Question.create :survey_id => @this_survey.id, :question_text => Faker::Lorem.sentence
 
-      4.times do
-        Choice.create :question_id => @this_question.id, :choice_text => Faker::Name.first_name
-      end
-    end
-  end
-end
+#       4.times do
+#         Choice.create :question_id => @this_question.id, :choice_text => Faker::Name.first_name
+#       end
+#     end
+#   end
+# end
