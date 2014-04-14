5aa3d848ea331908763819344a860aafeef6a3cd
Jonathan Wrobel <jwrobes@gmail.com>
2014-04-14 09:30:36 -0700

    added three new Survey tests 	*one test for matching questions to survey 	*one for choices to questions 	*one for the survey build method

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,30 +1,45 @@
 require 'spec_helper'
 
-describe Survey do
 
-  # let(:user) { FactoryGirl.create(:user) }
-  # before { @survey = user.surveys.build(desciption: "Lorem ipsum") }
+describe 'survey' do
 
-  # subject { @survey }
+  it "should save survey to database" do
+    survey = Survey.create(user_id: 1, title: "Test Survey", description: "test survey description")
+  expect(Survey.find_by_title("Test Survey").description).to eq('test survey description')
+end
 
-  #   it { should respond_to(:title) }
-  #   it { should respond_to(:description) }
-  #   it { should respond_to(:user_id) }
-  #   # it { should respond_to(:user) }
-  #   its(:user) { should eq user }
+  it "should hold questions with choices" do
+    survey2 = Survey.create(user_id: 1, title: "SampleSurvey", description: "describe")
+    question = Question.create(question_text: "Sample Question")
+    choice1 = Choice.create(choice_text: "Sample choice1")
+    choice2 = Choice.create(choice_text: "Sample choice2")
+    question.choices << choice1
+    question.choices << choice2
+    survey2.questions << question
 
-  # it { should be_valid }
+    expect(Survey.find_by_title("SampleSurvey").questions.first.choices.length).to eq(2)
+    Survey.last.destroy
+  end
 
-  # describe "when user_id is not present" do
-  #   before { @survey.user_id = nil }
-  #   it { should_not be_valid }
-  # end
-  # describe "with blank desciption" do
-  #   before { @survey.desciption = " " }
-  #   it { should_not be_valid }
-  # end
-  # describe "with desciption that is too long" do
-  #   before { @survey.description = "a" * 141 }
-  #   it { should_not be_valid }
-  # end
+  it "should hold questions with choices" do
+    survey2 = Survey.create(user_id: 1, title: "SampleSurvey", description: "describe")
+    question = Question.create(question_text: "Sample Question")
+    choice1 = Choice.create(choice_text: "Sample choice1")
+    choice2 = Choice.create(choice_text: "Sample choice2")
+    question.choices << choice1
+    question.choices << choice2
+    survey2.questions << question
+
+    expect(Survey.find_by_title("SampleSurvey").questions.first.choices.length).to eq(2)
+    Survey.last.destroy
+  end
+
+  it "should build questions and hashes with params" do
+      params = {"title" => "SampleSurvey", "description" =>"description", "question1" => "Samplequestion","question1choice1" =>"q1choice1", "question1choice2" => "q1choice2", "question1choice3" => "q1choice3"}
+      
+      test = Survey.create("title" => "SampleSurvey", "description" =>"description")
+    test.build(params)        
+    expect(test.questions.first.choices.length).to eq(3)
+  end
+  
 end
