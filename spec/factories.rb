875b82489e5d1dc2d71ff46891644a47ea28b0db
Alexandra Swart <answart@sbcglobal.net>
2014-04-14 11:34:52 -0700

    Comment out unprepared files for presentation

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,13 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:name)  { |n| "Person #{n}" }
-    sequence(:email) { |n| "person_121231#{n}@example.com"}
-    password_hash "foobar"
-  end
+# FactoryGirl.define do
+#   factory :user do
+#     sequence(:name)  { |n| "Person #{n}" }
+#     sequence(:email) { |n| "person_121231#{n}@example.com"}
+#     password_hash "foobar"
+#   end
 
-  factory :survey do
-    sequence(:title)  { |n| "Title #{n}" }
-    sequence(:description)  { |n| "Description #{n}" }
-    user
-  end
-end
+#   factory :survey do
+#     sequence(:title)  { |n| "Title #{n}" }
+#     sequence(:description)  { |n| "Description #{n}" }
+#     user
+#   end
+# end
