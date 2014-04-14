56295bcbd2366e35d3582b898ee0713254ee5da3
Alexandra Swart <answart@sbcglobal.net>
2014-04-13 18:26:08 -0700

    A User can search for other Users
    * Add a search link in the header.
    * Add a search form in search page.
    * Add a each loop to look for users names similar to searched name.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,3 +4,14 @@ get '/' do
   @surveys = Survey.all
   erb :index
 end
+
+get '/search/' do
+  erb :search
+end
+
+get '/search/*' do
+  @search_term = params[:name]
+  name = "%" + params[:name] + "%"
+  @searched_users = User.where('name like ?', name )
+  erb :search
+end
